38bf4dabb3aead0c813682def48103e398493ad0
Ulric Wilfred <shaman.sir@gmail.com>
2024-05-30 14:12:49 +0200

    draft for the grammar type

---
+++ src/Grammar.purs
@@ -0,0 +1,41 @@
+module Grammar where
+
+import Prelude
+
+import Data.Map (Map)
+
+
+type RuleName = String
+
+
+data Grammar = Grammar (Map RuleName Rule)
+
+
+data Rule
+    = Ref RuleName
+    | Range Char Char
+    | Sequence (Array Grammar)
+    | Switch (Array Grammar)
+    | Text String
+    | Char Char
+    | Not Grammar
+    | RepSep Grammar Grammar
+
+
+
+type Position = { start :: Int, end :: Int }
+
+
+data AST a
+    = Nil
+    | Node a (Array (AST a))
+    | Leaf RuleName Rule Position a
+
+
+
+data Failure = Failure Position
+
+
+
+parse :: forall a. String -> (Rule -> a) -> Grammar -> AST a
+parse _ _ _ = Nil
